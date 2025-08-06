b8c95507b231f4d5b082b423b98ad7f74d756bd7
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2025-08-06 20:06:10 -0300

    Add files via upload

---
+++ untitled/src/main/java/br/edu/univille/poo/Consulta.java
@@ -0,0 +1,30 @@
+package br.edu.univille.poo;
+
+
+import java.sql.*;
+
+
+public class Consulta
+{
+    public static void main( String[] args )
+    {
+        String password = "univille";
+        String url = "jdbc:mysql://localhost:3306/poo_2";
+        String user = "root";
+
+        try{
+            Connection con = DriverManager.getConnection(url,user,password);
+            String select = "SELECT id, nome, cpf FROM pessoa;";
+            PreparedStatement pre = con.prepareStatement(select);
+            ResultSet rs = pre.executeQuery();
+            while(rs.next()){
+                long id = rs.getLong(1);
+                String nome = rs.getString("nome");
+                String cpf = rs.getString("cpf");
+                System.out.println(id+", "+nome+" "+cpf);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
